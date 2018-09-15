ed5e8e7dfca1bdcac9f8544227a532325c3f440a
阿保達也 <pinog.jh@gmail.com>
2018-09-15 22:30:46 +0900

    タイミングバーが敵のバーに到達したら透過しながら消えるようにした

--- Assets/Abo/Scripts/UI/TimingBar.cs
+++ Assets/Abo/Scripts/UI/TimingBar.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 public class TimingBar : MonoBehaviour {
     //=============================================================
     private GameManager gameManager;
     private SoundManager soundManager;
+    private Image image;
 
     //=============================================================
     private Vector3 iniPos = new Vector3(-160,0,0);
@@ -15,11 +16,13 @@ public class TimingBar : MonoBehaviour {
     private int destroyCountLength = 1;
 
     //=============================================================
+    private bool destroyFlag;
+
     private int notesWave4;
     private int destroyCount;
 
     private float BPM;
-    private float time;
+    private float bgmTime;
 
 
     //=============================================================
@@ -27,15 +30,16 @@ public class TimingBar : MonoBehaviour {
         CRef();
 
         BPM = gameManager.TstBGMBPM;
-        time = soundManager.GetBGMTime("bgm001");
+        bgmTime = soundManager.GetBGMTime("bgm001");
 
-        notesWave4 = gameManager.GetBeatWaveNum(time,4,BPM);
+        notesWave4 = gameManager.GetBeatWaveNum(bgmTime,4,BPM);
     }
 
     //=============================================================
     private void CRef () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        image = GetComponent<Image>();
     }
 
     //=============================================================
@@ -45,24 +49,47 @@ public class TimingBar : MonoBehaviour {
 
     private void Update () {
         //時間の取得
-        time = soundManager.GetBGMTime("bgm001");
+        bgmTime = soundManager.GetBGMTime("bgm001");
 
         //位置の更新
         GetComponent<RectTransform>().localPosition = Vector3.Lerp(
             iniPos - (goalPos - iniPos) * (destroyCountLength - destroyCount),
             goalPos - (goalPos - iniPos) * (destroyCountLength - destroyCount),
-            gameManager.GetBeatWaveTiming(time,4,BPM)
+            gameManager.GetBeatWaveTiming(bgmTime,4,BPM)
             );
 
-        //消滅までのカウントを進める
-        if(notesWave4 != gameManager.GetBeatWaveNum(time,4,BPM)) {
-            notesWave4 = gameManager.GetBeatWaveNum(time,4,BPM);
-            destroyCount++;
+        if(!destroyFlag) {
+            //消滅までのカウントを進める
+            if(notesWave4 != gameManager.GetBeatWaveNum(bgmTime,4,BPM)) {
+                notesWave4 = gameManager.GetBeatWaveNum(bgmTime,4,BPM);
+                destroyCount++;
+            }
+
+            //カウントが進んだら消す
+            if(destroyCount == destroyCountLength + 1) {
+                destroyFlag = true;
+                StartCoroutine(DestroyRoutine());
+            }
+        }
+    }
+
+    //=============================================================
+    //オブジェクト破壊時の処理(アニメーション)
+    private IEnumerator DestroyRoutine () {
+        Color iniColor = image.color;
+        float time = 0;
+
+        while(true) {
+            time += Time.deltaTime;
+            if(time >= 1) {
+                break;
+            }
+
+            image.color = new Color(iniColor.r,iniColor.g,iniColor.b,1 - time);
+
+            yield return null;
         }
 
-        //カウントが進んだら消す
-        if(destroyCount == destroyCountLength + 1) {
-            Destroy(this.gameObject);
-        }
+        Destroy(this.gameObject);
     }
 }
