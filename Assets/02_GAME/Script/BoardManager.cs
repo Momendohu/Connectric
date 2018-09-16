bea39efb1971f6249044b5706beb47eed1f37daa
THS65104\nagata ryoya <naga-ryo0625@outlook.com>
2018-09-16 13:18:49 +0900

    パズルのリンク、消去、補充

--- Assets/02_GAME/Script/BoardManager.cs
+++ Assets/02_GAME/Script/BoardManager.cs
@@ -9,16 +9,26 @@ public class BoardManager : MonoBehaviour {
     public const int BOARD_WIDTH_NUM = 5;
     public const int BOARD_HEIGHT_NUM = 5;
     public const float between = 1.85f;
+    public Vector3 vStartPos = new Vector3(-3.7f, 1.2f, 0.0f);
 
     // ピースタイプ
     public enum INSTRUMENT_TYPE
-    {
+    { 
         GUITAR = 0,
         DRUM,
         VOCAL,
         DJ,
+        TIME,
         MAX
     };
+
+    // リンクターゲットタイプ
+    public enum TARGET_FORM
+    {
+        TATE = 0,
+        YOKO,
+        MAX
+    }
 
     // 構造体
     public struct PANEL_DATA
@@ -29,6 +39,7 @@ public class BoardManager : MonoBehaviour {
         public int typeNum;          // 属性
         public bool mouseFlag;       // キャプチャーされているか
         public bool moveFlag;        // 動かせるか
+        public bool linkflag;        // リンクしているかの確認
     };
 
     // 変数
@@ -40,38 +51,55 @@ public class BoardManager : MonoBehaviour {
     [SerializeField]private GameObject board;
     [SerializeField]private GameObject[] piece = new GameObject[(int)INSTRUMENT_TYPE.MAX];
     [SerializeField] private bool[] flag = new bool[BOARD_ALL_NUM];
+
+
+
+    [SerializeField]private int TargetForm = 0;     // ターゲット    
+    [SerializeField]public int combo = 0;           // コンボ数
+    [SerializeField]public bool deleteFrag = false;
+    private int[,] Target = new int[2,2];           // リンクテスト
     
-
-
 
     //===================================================
     // Use this for initialization
     //===================================================
     void Start () {
         CreateBoard();
-	}
+
+        // テスト
+        Target[0, 0] = (int)INSTRUMENT_TYPE.GUITAR;
+        Target[1, 0] = (int)INSTRUMENT_TYPE.DRUM;
+        Target[0, 1] = (int)INSTRUMENT_TYPE.VOCAL;
+        Target[1, 1] = (int)INSTRUMENT_TYPE.DJ;
+
+    }
 
     //===================================================
     // Update is called once per frame
     //===================================================
     void Update () {
 
+
         MoveMausePiece();
-        
-	}
+        LinkDo();
+
+        if (deleteFrag){ DeleteLink(); }
+
+        Replenishment();    // 補充
+
+
+    }
 
     //-------------------------------------------------------
     // 配置ボードの生成
     //-------------------------------------------------------
     private void CreateBoard()
     {
-       
-        Vector3 vStartPos = new Vector3(-3.7f, 1.2f, 0.0f);
         for (int height = 0; height < BOARD_HEIGHT_NUM; height++)
         {
             for (int width = 0; width < BOARD_WIDTH_NUM; width++)
             {
-                int obj_num = UnityEngine.Random.Range(0, (int)INSTRUMENT_TYPE.MAX);
+                int obj_num = UnityEngine.Random.Range(0, (int)INSTRUMENT_TYPE.MAX - 1);
 
                 // ボード（あたり判定）
                 Boards[width,height] = Instantiate(board, new Vector3(vStartPos.x + between * width, vStartPos.y - between * height, 0.0f), Quaternion.identity);
@@ -225,9 +253,7 @@ public class BoardManager : MonoBehaviour {
                         Boards[width, height - 1].GetComponent<SpriteRenderer>().color = Color.cyan;
                         Boards[width, height + 1].GetComponent<SpriteRenderer>().color = Color.cyan;
                     }
-
                     
-                    break;
                 }
             }
         }
@@ -325,7 +351,6 @@ public class BoardManager : MonoBehaviour {
         }
     }
 
-
     //-------------------------------------------------------
     // マウスが持っていたオブジェクトの解放
     //-------------------------------------------------------
@@ -344,10 +369,157 @@ public class BoardManager : MonoBehaviour {
         {
             for (int width = 0; width < BOARD_WIDTH_NUM; width++)
             {
-                Boardpieces[width, height].obj.GetComponent<Transform>().position = Boards[width, height].GetComponent<Transform>().position;
-                Boardpieces[width, height].obj.GetComponent<SpriteRenderer>().sortingOrder = 0;
-                Boardpieces[width, height].mouseFlag = false;
+                if(Boardpieces[width, height].mouseFlag)
+                {
+                    Boardpieces[width, height].obj.GetComponent<Transform>().position = Boards[width, height].GetComponent<Transform>().position;
+                    Boardpieces[width, height].obj.GetComponent<SpriteRenderer>().sortingOrder = 0;
+                    Boardpieces[width, height].mouseFlag = false;
+                    break;
+                }
+                
             }
         }
     }
+
+    //-------------------------------------------------------
+    // リンクチェック
+    //-------------------------------------------------------
+    private void LinkDo()
+    {
+        // ノーツリンクの取得
+        //Target = GetLinkData();
+
+
+        // 念のため初期化
+        for (int height = 0; height < (BOARD_HEIGHT_NUM - 1); height++)
+        {
+            for (int width = 0; width < BOARD_WIDTH_NUM; width++)
+            {
+                Boardpieces[width, height].linkflag = false;
+            }
+        }
+
+        // タイプの分岐
+        switch (TargetForm)
+        {
+            case (int)TARGET_FORM.TATE:
+                {
+                    combo = LinkTate();
+                    break;
+                }
+            case (int)TARGET_FORM.YOKO:
+                {
+                    combo = LinkYoko();
+                    break;
+                }
+            default: { break; }
+        }
+    }
+
+    //-------------------------------------------------------
+    // リンクチェック(縦)
+    //-------------------------------------------------------
+    private int LinkTate()
+    {
+        int linknum = 0;
+
+        for (int height = 0; height < (BOARD_HEIGHT_NUM - 1); height++)
+        {
+            for (int width = 0; width < BOARD_WIDTH_NUM; width++)
+            {
+                if (Boardpieces[width, height].typeNum == Target[0, 0])
+                {
+                    if(Boardpieces[width, height + 1].typeNum == Target[0, 1])
+                    {
+                        linknum++;
+                        Boardpieces[width, height].linkflag = true;
+                        Boardpieces[width, height + 1].linkflag = true;
+                        continue;
+                    }
+                }
+            }
+        }
+        return linknum;
+    }
+
+    //-------------------------------------------------------
+    // リンクチェック(横)
+    //-------------------------------------------------------
+    private int LinkYoko()
+    {
+        int linknum = 0;
+
+        for (int height = 0; height < BOARD_HEIGHT_NUM; height++)
+        {
+            for (int width = 0; width < (BOARD_WIDTH_NUM - 1); width++)
+            {
+                if (Boardpieces[width, height].typeNum == Target[0, 0])
+                {
+                    if (Boardpieces[width + 1, height].typeNum == Target[1, 0])
+                    {
+                        linknum++;
+                    }
+                }
+            }
+        }
+        return linknum;
+    }
+
+    //-------------------------------------------------------
+    // リンク削除
+    //-------------------------------------------------------
+    private void DeleteLink()
+    {
+        for (int height = 0; height < BOARD_HEIGHT_NUM; height++)
+        {
+            for (int width = 0; width < BOARD_WIDTH_NUM; width++)
+            {
+                if (Boardpieces[width, height].linkflag)
+                {
+                    Destroy(Boardpieces[width, height].obj);
+                    Boardpieces[width, height].obj = Instantiate(piece[(int)INSTRUMENT_TYPE.TIME], new Vector3(vStartPos.x + between * width, vStartPos.y - between * height, 0.0f), Quaternion.identity);
+                    Boardpieces[width, height].arrayWidthNum = width;
+                    Boardpieces[width, height].arrayHeightNum = height;
+                    Boardpieces[width, height].typeNum = (int)INSTRUMENT_TYPE.TIME;
+                    Boardpieces[width, height].mouseFlag = false;
+                    Boardpieces[width, height].moveFlag = false;
+
+                }
+            }
+        }
+
+        deleteFrag = false;
+    }
+
+
+    //-------------------------------------------------------
+    // 補充
+    //-------------------------------------------------------
+    private void Replenishment()
+    {
+        for (int height = 0; height < BOARD_HEIGHT_NUM; height++)
+        {
+            for (int width = 0; width < BOARD_WIDTH_NUM; width++)
+            {
+                if (Boardpieces[width, height].typeNum == (int)INSTRUMENT_TYPE.TIME)
+                {
+                    if (Boardpieces[width, height].obj.GetComponent<PieceTime>().GetFinAnim())
+                    {
+                        Destroy(Boardpieces[width, height].obj);
+
+                        int obj_num = UnityEngine.Random.Range(0, (int)INSTRUMENT_TYPE.MAX - 1);
+
+                        // ピース
+                        Boardpieces[width, height].obj = Instantiate(piece[obj_num], new Vector3(vStartPos.x + between * width, vStartPos.y - between * height, 0.0f), Quaternion.identity);
+                        Boardpieces[width, height].arrayWidthNum = width;
+                        Boardpieces[width, height].arrayHeightNum = height;
+                        Boardpieces[width, height].typeNum = obj_num;
+                        Boardpieces[width, height].mouseFlag = false;
+                        Boardpieces[width, height].moveFlag = false;
+                    }
+                }
+            }
+        }
+    }
+
 }
