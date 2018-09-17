0121fc2e20236e0b95d38249bdea2fe426b7a838
阿保達也 <pinog.jh@gmail.com>
2018-09-18 03:44:26 +0900

    キャラクターセレクトでキャラクターの画像も変化させるように

--- Assets/Abo/Scripts/CharacterSelect/CharacterSelectUI.cs
+++ Assets/Abo/Scripts/CharacterSelect/CharacterSelectUI.cs
@@ -7,6 +7,7 @@ public class CharacterSelectUI : MonoBehaviour {
     //=============================================================
     private GameManager gameManager;
 
+    private Image characterImage; //キャラクターの画像
     private Text nameAndLV; //名前とレベル
     private Text skillDescription; //スキル説明
     private Image instrumentTypeIcon; //楽器タイプ
@@ -23,6 +24,7 @@ public class CharacterSelectUI : MonoBehaviour {
     //=============================================================
     private void CRef () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        characterImage = transform.Find("Character").GetComponent<Image>();
         nameAndLV = transform.Find("Image/Text").GetComponent<Text>();
         skillDescription = transform.Find("Image2/Text").GetComponent<Text>();
         instrumentTypeIcon = transform.Find("Type/Icon").GetComponent<Image>();
@@ -39,6 +41,8 @@ public class CharacterSelectUI : MonoBehaviour {
 
     private void Update () {
         GameManager.CharacterData characterData = gameManager.CharacterDatas[0];
+
+        characterImage.sprite = gameManager.CharacterImage[characterData.Id];
         nameAndLV.text = characterData.Name + " LV999";
         skillDescription.text = "ActiveSkill - " + characterData.ActiveSkill + "\nPassiveSkill - " + characterData.PassiveSkill;
         instrumentTypeIcon.sprite = gameManager.PieceLinkImage[(int)characterData.InstrumentType];
