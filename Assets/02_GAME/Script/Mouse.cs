8d7fe9426be3bba650f2ad0fbb3975b03a807ddc
阿保達也 <pinog.jh@gmail.com>
2018-09-19 00:27:10 +0900

    mouse

--- Assets/02_GAME/Script/Mouse.cs
+++ Assets/02_GAME/Script/Mouse.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour {
 
-    [SerializeField]private  float cursol_X;
-    [SerializeField]private  float cursol_Y;
-    [SerializeField]private  bool tapFlag;
-    [SerializeField]private  bool oldTapFlag;
-    [SerializeField]private  bool is_TriggerTapFlag;
-    [SerializeField]private  bool is_ReleaseTapFlag;
-    [SerializeField]private GameObject board;
-    [SerializeField]public static bool ChaptureFlag;
+    private const float RIMIT_TOP = 2.5f;
+    private const float RIMIT_BOTTOM = -7.0f;
+    private const float RIMIT_LEFT = -4.8f;
+    private const float RIMIT_RIGHT = 4.8f;
+
+
+    [SerializeField] private float cursol_X;
+    [SerializeField] private float cursol_Y;
+    [SerializeField] private bool tapFlag;
+    [SerializeField] private bool oldTapFlag;
+    [SerializeField] private bool is_TriggerTapFlag;
+    [SerializeField] private bool is_ReleaseTapFlag;
+    [SerializeField] private GameObject board;
+    [SerializeField] public static bool CaptureFlag;
 
     // Use this for initialization
     void Start () {
         oldTapFlag = false;
         tapFlag = false;
-        ChaptureFlag = false;
+        CaptureFlag = false;
         MouseInfo();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate () {
 
         MouseInfo();
 
-        if(is_ReleaseTapFlag) { ChaptureFlag = false; }
+        if(cursol_X < RIMIT_LEFT || cursol_X > RIMIT_RIGHT || cursol_Y < RIMIT_BOTTOM || cursol_Y > RIMIT_TOP || is_ReleaseTapFlag) {
+            CaptureFlag = false;
+        }
+        Debug.Log(CaptureFlag);
+
     }
 
     //---------------------------------------------------------
     // マウスの座標やクリックの管理
     //---------------------------------------------------------
-    private void MouseInfo()
-    {
+    private void MouseInfo () {
 
-        Vector3 vPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+        Vector3 vPos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,0.0f);
         Vector3 vWorldPos = Camera.main.ScreenToWorldPoint(vPos);
         cursol_X = vWorldPos.x;
         cursol_Y = vWorldPos.y;
@@ -44,39 +53,35 @@ public class Mouse : MonoBehaviour {
         is_TriggerTapFlag = !oldTapFlag && tapFlag; // トリガー
         is_ReleaseTapFlag = oldTapFlag && !tapFlag; // リリース 
         oldTapFlag = tapFlag;                       // 過去のクリック
-        this.transform.position = new Vector3(cursol_X, cursol_Y, vWorldPos.z);
+        this.transform.position = new Vector3(cursol_X,cursol_Y,vWorldPos.z);
     }
 
     //--------------------------------------------------------
     // 侵入検知(領域に入っているとき)
     //--------------------------------------------------------
-    void OnTriggerStay2D(Collider2D other)
-    {
+    void OnTriggerStay2D (Collider2D other) {
         // クリック時一度のみ
-        if(is_TriggerTapFlag)
-        {
+        if(is_TriggerTapFlag) {
             // ボードのキャプチャー
-            if (other.tag == "Board")
-            {
-                ChaptureFlag = true;
+            if(other.tag == "Board") {
+                if(cursol_X > RIMIT_LEFT && cursol_X < RIMIT_RIGHT && cursol_Y > RIMIT_BOTTOM & cursol_Y < RIMIT_TOP) {
+                    CaptureFlag = true;
+                }
                 other.GetComponent<SpriteRenderer>().color = new Color(0.5f,0.5f,0.5f,0.0f);
                 board.GetComponent<BoardManager>().SetMouseObj();
             }
         }
 
         // クリックしている間
-        if(tapFlag && !is_TriggerTapFlag)
-        {
-            if(other.tag == "Board")
-            {
+        if(tapFlag && !is_TriggerTapFlag) {
+            if(other.tag == "Board") {
                 other.GetComponent<SpriteRenderer>().color = Color.black;
                 board.GetComponent<BoardManager>().Change();
             }
         }
 
         // リリースした時
-        if(is_ReleaseTapFlag)
-        {
+        if(/*is_ReleaseTapFlag && */!CaptureFlag) {
             board.GetComponent<BoardManager>().ReleaseMouseObj();
         }
 
