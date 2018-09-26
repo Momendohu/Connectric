8eb8359a2e95ff101d8508619ec69abf618427c2
THS65104\nagata ryoya <naga-ryo0625@outlook.com>
2018-09-26 23:22:16 +0900

    リザルトからセレクトへ

--- Assets/04_Result/ResultPre/ResultManager.cs
+++ Assets/04_Result/ResultPre/ResultManager.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class ResultManager : MonoBehaviour {
 
-    // サウンド
 
-    // ゲームマネージャー
-
+    private GameObject gamemanager;
 
 	// Use this for initialization
 	void Start () {
-
-
-		
+        gamemanager = GameObject.Find("GameManager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        // シーン遷移
-        if(Input.GetMouseButtonDown(0))
-        {
-
-        }
+        
 		
 	}
+
+
+    // 
+    // マウス
+    //----------------------------------------
+    public void OnClick()
+    {
+       gamemanager.GetComponent<GameManager>().JumpSceneResultToMusicSelect();
+    }
 }
