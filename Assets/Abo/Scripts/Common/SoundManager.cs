533ea5949fef6061cd9122a5670c530dd8d3a921
阿保達也 <pinog.jh@gmail.com>
2018-09-29 16:14:46 +0900

    ボリューム設定の仕組み

--- Assets/Abo/Scripts/Common/SoundManager.cs
+++ Assets/Abo/Scripts/Common/SoundManager.cs
@@ -88,6 +88,45 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager> {
     }
 
     //===============================================================================
+    //オーディオを鳴らす、ボリューム設定可能
+    public void TriggerBGM (string name,bool isUseLoop,float volume) {
+        Debug.Log(name);
+        //SoundManagerにアタッチしてあるものと照合
+        //指定したものがなければ再生しない
+        int bgmListNum = CheckMatchNameInList(name,BGMList);
+        if(bgmListNum != -1) {
+
+            //すでに生成してあるオブジェクトと照合
+            //すでにあるならそれを再生
+            //ないならオブジェクト生成して再生
+            int bgmObjNum = CheckMatchNameInList(name,BGMObject);
+            if(bgmObjNum != -1) {
+                BGMObject[bgmObjNum].GetComponent<AudioSource>().volume = volume; //ボリュームの設定
+                BGMObject[bgmObjNum].GetComponent<AudioSource>().Play();
+            } else {
+
+                //オーディオ再生用の子オブジェクトを作成
+                GameObject obj = Instantiate(Resources.Load("Prefabs/Other/SoundManagerAudio")) as GameObject;
+                obj.name = name;
+                BGMObject.Add(obj);
+                obj.transform.SetParent(this.transform);
+
+                //AudioSourceにAudioClipをアタッチ
+                obj.GetComponent<AudioSource>().clip = BGMList[bgmListNum];
+
+                //ボリュームの設定
+                obj.GetComponent<AudioSource>().volume = volume;
+
+                //再生
+                obj.GetComponent<AudioSource>().Play();
+                obj.GetComponent<AudioSource>().loop = isUseLoop;
+            }
+        } else {
+            Debug.Log("指定したAudioClipが無いよ");
+        }
+    }
+
+    //===============================================================================
     //オーディオを鳴らす(独立して鳴らす)
     public void TriggerSE (string name) {
 
@@ -113,6 +152,45 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager> {
 
                 //AudioSourceにAudioClipをアタッチ
                 obj.GetComponent<AudioSource>().clip = SEList[seListNum];
+
+                //再生
+                obj.GetComponent<AudioSource>().PlayOneShot(SEList[seListNum]);
+            }
+        } else {
+            Debug.Log("指定したAudioClipが無いよ");
+        }
+    }
+
+    //===============================================================================
+    //オーディオを鳴らす(独立して鳴らす)、ボリューム設定可能
+    public void TriggerSE (string name,float volume) {
+
+        //SoundManagerにアタッチしてあるものと照合
+        //指定したものがなければ再生しない
+        int seListNum = CheckMatchNameInList(name,SEList);
+        if(seListNum != -1) {
+
+            //すでに生成してあるオブジェクトと照合
+            //すでにあるならそれを再生
+            //ないならオブジェクト生成して再生
+            int seObjNum = CheckMatchNameInList(name,SEObject);
+            if(seObjNum != -1) {
+                AudioSource audioSource = SEObject[seObjNum].GetComponent<AudioSource>();
+                audioSource.volume = volume; //ボリュームの設定
+                audioSource.PlayOneShot(audioSource.clip);
+            } else {
+
+                //オーディオ再生用の子オブジェクトを作成
+                GameObject obj = Instantiate(Resources.Load("Prefabs/Other/SoundManagerAudio")) as GameObject;
+                obj.name = name;
+                SEObject.Add(obj);
+                obj.transform.SetParent(this.transform);
+
+                //AudioSourceにAudioClipをアタッチ
+                obj.GetComponent<AudioSource>().clip = SEList[seListNum];
+
+                //ボリュームの設定
+                obj.GetComponent<AudioSource>().volume = volume;
 
                 //再生
                 obj.GetComponent<AudioSource>().PlayOneShot(SEList[seListNum]);
