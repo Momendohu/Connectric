bd390b72e3cbdcff01afa18b555153b00bc1367a
阿保達也 <pinog.jh@gmail.com>
2018-10-26 11:57:39 +0900

    ゲームスタート表示追加

---
+++ Assets/Abo/Scripts/Game/UI/GameStartText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStartText : MonoBehaviour {
+    //=============================================================
+    private Text mainText;
+    private Text shadowText;
+
+    //=============================================================
+    private void Init () {
+        CRef();
+    }
+
+    //=============================================================
+    private void CRef () {
+        mainText = transform.Find("Main").GetComponent<Text>();
+        shadowText = transform.Find("Shadow").GetComponent<Text>();
+    }
+
+    //=============================================================
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+        StartCoroutine(Disappear(2,2));
+    }
+
+    //=============================================================
+    //消滅
+    private IEnumerator Disappear (float waitTime,float speed) {
+        float time = 0;
+        while((waitTime - time) >= 0) {
+            time += Time.fixedDeltaTime * speed;
+
+            mainText.color = new Color(1,1,1,waitTime - time);
+            shadowText.color = new Color(0,0,0,waitTime - time);
+
+            yield return null;
+        }
+
+        mainText.color = new Color(1,1,1,0);
+        shadowText.color = new Color(0,0,0,0);
+
+        Destroy(this.gameObject);
+    }
+}
